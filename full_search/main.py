5222ee2f7df34f07de01c1dd2c5e65b00fdbcfb5
ZXCotic <152020290+ZXCotic@users.noreply.github.com>
2025-05-18 12:42:35 +0300

    Update main.py

--- full_search/main.py
+++ full_search/main.py
@@ -1,78 +1,60 @@
+import requests
 import sys
+from PIL import Image
 from io import BytesIO
 
-import requests
-from PIL import Image
+def get_spn(toponym_json: dict) -> str:
+    bounds = toponym_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["boundedBy"]["Envelope"]
+    lower_corner_x, lower_corner_y = map(float, bounds["lowerCorner"].split())
+    upper_corner_x, upper_corner_y = map(float, bounds["upperCorner"].split())
+    x_long = str(round(upper_corner_x - lower_corner_x))
+    y_long = str(upper_corner_y - lower_corner_y)
+    return ",".join([x_long, y_long])
 
-from utils import get_address_span
-
-
-
-def get_geocode_result(geocode_data, **params):
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
+def fetch_geocoder_data(toponym_to_find: str) -> dict:
+    geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
     geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": geocode_data,
-        "format": "json",
-        **params
+        "geocode": toponym_to_find,
+        "format": "json"
     }
-
     response = requests.get(geocoder_api_server, params=geocoder_params)
+    response.raise_for_status()
+    return response.json()
 
-    if not response:
-        pass
-    json_response = response.json()
-
-    return json_response
-
-
-def get_toponym(geocode_result):
-    try:
-        toponym = geocode_result["response"]["GeoObjectCollection"][
-            "featureMember"][0]["GeoObject"]
-    except (KeyError, IndexError):
-        raise ValueError
-    return toponym
-
-
-def get_ll_from_geocode_response(toponym):
-    toponym_coodrinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-    return toponym_longitude, toponym_lattitude
-
-
-def get_static_map(lat, lon, **params):
-    map_type = params.get('l', 'map')
-    map_params = {
-        "ll": ",".join([lat, lon]),
-        "l": map_type,
-        **params,
+def fetch_static_map(toponym_coords: str, spn: str) -> bytes:
+    static_api_server = "https://static-maps.yandex.ru/v1"
+    static_params = {
+        "ll": toponym_coords,
+        "spn": spn,
+        "maptype": "map",
+        "pt": f'{toponym_coords},round',
     }
-    map_api_server = "http://static-maps.yandex.ru/1.x/"
-    response = requests.get(map_api_server, params=map_params)
+    response = requests.get(static_api_server, params=static_params)
+    response.raise_for_status()
     return response.content
-
 
 def main():
     toponym_to_find = " ".join(sys.argv[1:])
-    geocode_res = get_geocode_result(toponym_to_find)
+
+    if not toponym_to_find:
+        print("Ошибка: Необходимо ввести название места.")
+        sys.exit(1)
+
     try:
-        toponym = get_toponym(geocode_res)
-    except ValueError:
-        print('Объект не найден')
-        return 1
-    lat, lon = get_ll_from_geocode_response(toponym)
-    span = get_address_span(toponym)
-    span = ','.join(map(str, span))
-    pointer_style = 'pm2'
-    pointer_color = 'org'
-    pointer_size = 'l'
-    pointer = f'{lat},{lon},{pointer_style}{pointer_color}{pointer_size}'
-    image_raw = get_static_map(lat, lon, spn=span, pt=pointer)
-    Image.open(BytesIO(
-        image_raw)).show()
+        json_geocoder_response = fetch_geocoder_data(toponym_to_find)
+        toponym = json_geocoder_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        toponym_coords = toponym["Point"]["pos"]
+        spn = get_spn(json_geocoder_response)
 
+        static_map_content = fetch_static_map(toponym_coords, spn)
+        Image.open(BytesIO(static_map_content)).show()
 
-if __name__ == '__main__':
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка сети: {e}")
+        sys.exit(1)
+    except (KeyError, IndexError) as e:
+        print(f"Ошибка обработки данных: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
     main()
